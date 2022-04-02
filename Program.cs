eeaedab4f563137c9a2aeb9bac0e394534bacaf5
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-02 15:08:52 -0300

    Listo

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    bool flag = false;
+do
+{
+    Console.WriteLine("Por favor presione una tecla.");
+    ConsoleKeyInfo key = Console.ReadKey();
+    Console.WriteLine("Usted presiono " + key.KeyChar);
+    Console.WriteLine("¿Quiere seguir preseionando teclas? En caso negativo presione 'X'.");
+    if (key.Key != ConsoleKey.X)
+    {
+        flag = true;
+    }
+    else
+    {
+        Console.WriteLine("Usted presiono " + key.KeyChar + ". En segundos el programa se cerrará.");
+        flag= false;
+    }
+
+} while (flag == true); 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
